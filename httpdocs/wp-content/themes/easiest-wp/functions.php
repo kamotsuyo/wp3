52a51060bd4b3943cb7899079aad2556ab23ea11
kamo(mac) <kamotsuyo@gmail.com>
2018-04-29 18:15:12 +0900

    functions.php記述したが、internal server error 発生している

--- httpdocs/wp-content/themes/easiest-wp/functions.php
+++ httpdocs/wp-content/themes/easiest-wp/functions.php
@@ -1,1 +1,6 @@
 <?PHP
+//オリジナル関数 stylesheetload_scripts を作成
+function k_load_script(){
+    wp_enqueue_style('my_mainstyle',get_stylesheet_directory_uri());
+}
+add_action('wp_enqueue_scripts','k_load_script');
