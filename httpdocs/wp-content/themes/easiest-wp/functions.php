29579c883cae6e4b4c97d9b92c8af4a3e94b7c78
kamo(mac) <kamotsuyo@gmail.com>
2018-04-29 22:05:21 +0900

    wp-setting default_time_set 修正
    エラーログがすべてUTCで出力されるので修正した。

--- httpdocs/wp-content/themes/easiest-wp/functions.php
+++ httpdocs/wp-content/themes/easiest-wp/functions.php
@@ -1,7 +1,8 @@
 <?PHP
 
 function k_load_scripts(){
-    wp_enqueue_style(‘mainstyle’ , get_stylesheet_uri());
+//    wp_enqueue_style(‘mainstyle’ , get_stylesheet_uri());
+    wp_enqueue_style(‘style.css’ , get_stylesheet_directory_uri());
 }
 
 add_action('wp_enqueue_scripts' , 'k_load_scripts');
