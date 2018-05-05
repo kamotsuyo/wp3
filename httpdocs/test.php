d6c98341cbf7f82bbe3761a6c0b3ee66effcf894
kamo(mac) <kamotsuyo@gmail.com>
2018-05-05 19:18:59 +0900

    wordpressからdbに接続するwpdbクラスの使用テスト

---
+++ httpdocs/test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+wordpressからdb接続のテスト
+接続方法は wpdbクラスを使用する
+参考:https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/wpdb_Class#.E5.88.97.E6.83.85.E5.A0.B1.E3.81.AE.E5.8F.96.E5.BE.97
+
+*/
+require_once('wp-load.php');
+
+global $wpdb;
+
+$results=$wpdb->get_results("select * from wp_posts where post_status = 'publish' and post_type = 'post' ");
+
+for($i=0; $i<count($results);$i++){
+    $low = $results[$i];
+    echo $low->post_title .  "\n". $low->post_type . '<br/>' ;
+}
